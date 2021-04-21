3a090ddd88f62253e1f625164fa6c9f95d7cebeb
xiaohe <chengxiaohe_1986@163.com>
2021-04-21 23:26:24 +0800

    1

--- 49+陈晓河+深圳/week2/test.py
+++ 49+陈晓河+深圳/week2/test.py
@@ -1,1 +1,12 @@
+#coding:utf8
+import numpy as np
+A = [1,2,3,4]
+B = [3,4,5,6]
+# print(np.dot(A,B))
+# print(np.linalg.norm(A) * np.linalg.norm(B))
+def Cosine_simi(A,B):
+    x = np.dot(A,B)
+    y = np.linalg.norm(A) * np.linalg.norm(B)
+    print(x / y)
 
+Cosine_simi(A,B)
